eee2280977dcf200530c4e23cce21bc07e453e77
Jefferson <jedoljak@gmail.com>
2023-07-13 00:29:19 +0000

    adiciona arquivos iniciais

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home () {
+    return [
+        <h4>Mô 😍, a caminhada pode ser longa ...</h4>,
+        <h3>... mas pode ser extremamente satisfatória ...</h3>,
+        <h2>... quando damos nossos ...</h2>,
+        <h1>... pequenos GRANDES🚀 passos!😘</h1>
+
+    ]
+}
+export default Home;
