eca489146d3e556b175ca138d621f9147e77ffc5
Jefferson <jedoljak@gmail.com>
2023-07-18 00:12:48 +0000

    adiona  e

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-function Home () {
-    return [
-        <h4>Mô 😍, a caminhada pode ser longa, ...</h4>,
-        <h3>... mas pode ser extremamente satisfatória, ...</h3>,
-        <h2>... quando damos nossos, ...</h2>,
-        <h1>... pequenos GRANDES🚀 passos!😘</h1>
-
-    ]
+function Home() {
+  return [
+    <h4>Mô 😍, a caminhada pode ser longa, ...</h4>,
+    <h3>... mas pode ser extremamente satisfatória, ...</h3>,
+    <h2>... quando damos nossos, ...</h2>,
+    <h1>... pequenos GRANDES🚀 passos!😘</h1>,
+  ];
 }
+
 export default Home;
